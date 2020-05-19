8eb206c60f83ab742d00aa94a1e5976dcb5c9060
vabarnikova <v.abarnikova@student.csn.khai.edu>
2020-05-19 23:05:53 +0300

    Release Labwork6 - UWP Application

---
+++ SystemProgrammingLab6/Calculator.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SystemProgrammingLab6
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public enum Operations
+    {
+        Add = 0,
+        Sub = 1,
+        Mul = 2,
+        Div = 3,
+        Sin = 4,
+        Cos = 5,
+        Tan = 6,
+        Sqrt = 7,
+        Log = 8,
+        Pow = 9
+    }
+    public sealed partial class Calculator : Page
+    {
+        private Operations _selectedOperation;
+        public Calculator()
+        {
+            this.InitializeComponent();
+        }
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            double x1, x2;
+
+            Result.Text = "";
+
+            if (!Double.TryParse(A.Text, out x1))
+            {
+                Result.Text = "Операнд х1 не число!";
+                return;
+            }
+
+            if (!Double.TryParse(B.Text, out x2))
+            {
+                Result.Text = "Операнд х2 не число!";
+                return;
+            }
+
+            switch (_selectedOperation)
+            {
+                case Operations.Add: Result.Text = (x1 + x2).ToString(); break;
+                case Operations.Sub: Result.Text = (x1 - x2).ToString(); break;
+                case Operations.Mul: Result.Text = (x1 * x2).ToString(); break;
+                case Operations.Div: Result.Text = (x2 == 0 ? "Нельзя делить на ноль" : (x1 / x2).ToString()); break;
+                case Operations.Sin: Result.Text = (Math.Sin(x1)).ToString(); break;
+                case Operations.Cos: Result.Text = (Math.Cos(x1)).ToString(); break;
+                case Operations.Tan: Result.Text = (Math.Tan(x1)).ToString(); break;
+                case Operations.Sqrt: Result.Text = (x1 < 0 ? "Значение не может быть меньше нуля!" : (Math.Sqrt(x1)).ToString()); break;
+                case Operations.Log: Result.Text = (Math.Log(x1)).ToString(); break;
+                case Operations.Pow: Result.Text = (Math.Pow(x1, x2)).ToString(); break;
+            }
+        }
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton pressed = (RadioButton)sender;
+            if (pressed.Tag == null)return;
+            _selectedOperation = (Operations)pressed.Tag;
+        }
+    }
+}
+
---
+++ SystemProgrammingLab6/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace SystemProgrammingLab6
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private const string AboutDeveloperChoice = "AboutDeveloper";
+        private const string CalculatorChoice = "Calculator";
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private void NavigationView_OnSelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            switch (((NavigationViewItem)args.SelectedItem).Tag.ToString())
+            {
+                case AboutDeveloperChoice:
+                    Frame.Navigate(typeof(AboutDeveloper));
+                    break;
+                case CalculatorChoice:
+                    Frame.Navigate(typeof(Calculator));
+                    break;
+                default: break;
+            }
+        }
+        private void MainPage_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            AboutDeveloper.IsSelected = true;
+        }
+    }
+}
